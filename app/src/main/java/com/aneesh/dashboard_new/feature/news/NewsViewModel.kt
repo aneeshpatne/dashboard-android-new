991e411fca2459787b3865d1698a7fffd9eb51f5
aneeshpatne <aneeshpatne@gmail.com>
2026-02-25 12:44:59 +0530

    Refactor NewsViewModel state to use NewsItems wrapper

--- app/src/main/java/com/aneesh/dashboard_new/feature/news/NewsViewModel.kt
+++ app/src/main/java/com/aneesh/dashboard_new/feature/news/NewsViewModel.kt
@@ -19,25 +19,31 @@ data class NewsItem(
     val ogUrl: String,
 )
 
+data class NewsItems(
+    val items: List<NewsItem>
+)
+
 class NewsViewModel : ViewModel() {
     private val _newsItems = MutableStateFlow(
-        listOf(
-            NewsItem(
-                id = "60189acd-fcf7-4c18-a23c-cd6a7295a7ad",
-                title = "SC exchange on ED probe of Mamata",
-                description = "In Supreme Court hearing over IPAC searches, ASG said ED was terrorised; case posted to March 18.",
-                sources = listOf(
-                    Sources(
-                        url = "https://www.livelaw.in/top-stories/ed-has-been-weaponised-no-ed-was-terrorised-exchange-in-supreme-court-plea-against-mamata-banerjee-523599",
-                        domain = "livelaw",
-                        ogImage = "https://www.livelaw.in/h-upload/2026/01/15/646967-mamata-banerjee-sc-ed.jpg",
+        NewsItems(
+            items = listOf(
+                NewsItem(
+                    id = "60189acd-fcf7-4c18-a23c-cd6a7295a7ad",
+                    title = "SC exchange on ED probe of Mamata",
+                    description = "In Supreme Court hearing over IPAC searches, ASG said ED was terrorised; case posted to March 18.",
+                    sources = listOf(
+                        Sources(
+                            url = "https://www.livelaw.in/top-stories/ed-has-been-weaponised-no-ed-was-terrorised-exchange-in-supreme-court-plea-against-mamata-banerjee-523599",
+                            domain = "livelaw",
+                            ogImage = "https://www.livelaw.in/h-upload/2026/01/15/646967-mamata-banerjee-sc-ed.jpg",
+                        ),
                     ),
+                    ogUrl = "https://www.livelaw.in/h-upload/2026/01/15/646967-mamata-banerjee-sc-ed.jpg",
                 ),
-                ogUrl = "https://www.livelaw.in/h-upload/2026/01/15/646967-mamata-banerjee-sc-ed.jpg",
-            ),
+            )
         )
     )
 
-    val newsItems: StateFlow<List<NewsItem>> = _newsItems.asStateFlow()
+    val newsItems: StateFlow<NewsItems> = _newsItems.asStateFlow()
 
 }
