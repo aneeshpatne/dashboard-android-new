2428f00bdc8a9997ffeb6d7fb507a18a5437adae
aneeshpatne <aneeshpatne@gmail.com>
2026-02-25 17:46:51 +0530

    Fix LazyRow AsyncImage call by adding contentDescription

--- app/src/main/java/com/aneesh/dashboard_new/feature/news/NewsScreen.kt
+++ app/src/main/java/com/aneesh/dashboard_new/feature/news/NewsScreen.kt
@@ -1,23 +1,28 @@
 package com.aneesh.dashboard_new.feature.news
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+
 @Composable
 fun NewsRoute(
     viewModel: NewsViewModel = viewModel()
@@ -58,6 +63,27 @@ fun NewsItemCard(newsItem: NewsItem){
         Column {
             Text(text = newsItem.title)
             Text(text = newsItem.description)
+            LazyRow {
+                items(newsItem.sources){
+                    SourceItem(it)
+                }
+            }
         }
+    }
+}
+
+@Composable
+fun SourceItem(sources: Sources){
+    Row (
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(6.dp),
+        modifier = Modifier.padding(2.dp)
+    ){
+        AsyncImage(
+            model = "https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${sources.url}&size=32",
+            contentDescription = sources.domain,
+            modifier = Modifier.size(16.dp)
+        )
+        Text(text = sources.domain)
     }
 }
